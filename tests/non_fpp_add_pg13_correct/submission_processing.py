4da58e041e95dd5dd61bd714f088395eaeddb228
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2023-06-15 16:33:10 -0700

    retrieve only script.rb contents

--- tests/non_fpp_add_pg13_correct/submission_processing.py
+++ tests/non_fpp_add_pg13_correct/submission_processing.py
@@ -1,10 +1,10 @@
 from base64 import b64decode
 from typing import Dict
 
-def get_submission(data: Dict):
-    """Extract the file contents in a student submission"""
-    contents = "\n".join([
-            file["contents"]
-            for file in data['submitted_answers']['_files']
-        ])
+def get_submission(data: Dict) -> str:
+    """Extract the file contents of `script.rb` in a student submission"""
+    contents = next(
+        ( file['contents' for file in data['submitted_answers'] if file['name'] == 'script.rb' ), 
+        ""
+    )
     return str(b64decode(contents), "utf-8")
